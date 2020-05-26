322f510004d89207650d3fcde2e7e583b0ff034f
mattocci <mattocci27@gmail.com>
2020-05-26 09:49:45 +0800

    clean

--- test_stan2.sh
+++ test_stan2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 SPAB=50
 DRY=dry
-MODEL=model_ind
+MODEL=model_census_fix
 echo "Model for ${DRY} season. Min. species abundance = ${SPAB}"
 echo "Run ${MODEL}.stan"
 
-export SPAB DRY
+export SPAB DRY MODEL
   # n_iter n_warm n_thin n_chain adapt_delta n_ab season
 R --vanilla --slave --args ${MODEL} 2 1 1 1 0.95 ${SPAB} ${DRY} < run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}.log
 #R --vanilla --slave --args ${MODEL} 3000 2000 1 4 0.95 ${SPAB} ${DRY} < run_stan.r > ./log/${DRY}_stan_${SPAB}.log
