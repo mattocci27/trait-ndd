ed7fc7934f6890aeb43b54f6c309c739434493e3
Masatoshi Katabuchi <mattocci27@gmail.com>
2020-06-15 18:31:35 +0800

    trait model

--- sh/model_ind.sh
+++ sh/model_ind.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 SPAB=50
-MODEL=model_ind
-DRY=dry
-for NEIGHBOR in full seedling adult
+MODEL=model_ind_gq
+for DRY in dry wet
 do
-  echo "Model for ${DRY} season. Min. species abundance = ${SPAB}"
-  echo "Run ${MODEL}.stan"
-  echo "Use ${NEIGHBOR} data"
-  export SPAB DRY MODEL NEIGHBOR
-    # n_iter n_warm n_thin n_chain adapt_delta n_ab season
-  nohup R --vanilla --slave --args ${MODEL} 2 1 1 1 0.95 ${SPAB} ${DRY} ${NEIGHBOR}< run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}.log &
-  sleep 1 # pause to be kind to the scheduler
-done
-wait
-
-DRY=wet
-for NEIGHBOR in full seedling adult
-do
-  echo "Model for ${DRY} season. Min. species abundance = ${SPAB}"
-  echo "Run ${MODEL}.stan"
-  echo "Use ${NEIGHBOR} data"
-  export SPAB DRY MODEL NEIGHBOR
-    # n_iter n_warm n_thin n_chain adapt_delta n_ab season
-  nohup R --vanilla --slave --args ${MODEL} 2 1 1 1 0.95 ${SPAB} ${DRY} ${NEIGHBOR}< run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}.log &
-  sleep 1 # pause to be kind to the scheduler
+  for TRAIT in C13 SLA WP PCA
+  do 
+    for NEIGHBOR in full seedling adult
+    do
+      echo "Model for ${DRY} season. Min. species abundance = ${SPAB}"
+      echo "Run ${MODEL}.stan"
+      echo "Use ${NEIGHBOR} data"
+      echo "Use ${TRAIT} for sp-level"
+      export SPAB DRY MODEL NEIGHBOR
+        # n_iter n_warm n_thin n_chain adapt_delta n_ab season
+      nohup R --vanilla --slave --args ${MODEL} 2 1 1 1 0.95 ${SPAB} ${DRY} ${NEIGHBOR} ${TRAIT}< run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}_${TRAIT}.log &
+      sleep 1 # pause to be kind to the scheduler
+    done
+    wait
+  done
+  wait
 done
 wait
--- test_stan2.sh
+++ test_stan2.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 SPAB=50
 DRY=dry
-MODEL=model_ind
+MODEL=model_ind_gq
+NEIGHBOR=adult
 echo "Model for ${DRY} season. Min. species abundance = ${SPAB}"
 echo "Run ${MODEL}.stan"
 
 export SPAB DRY MODEL
   # n_iter n_warm n_thin n_chain adapt_delta n_ab season
-R --vanilla --slave --args ${MODEL} 2 1 1 1 0.95 ${SPAB} ${DRY} < run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}.log
+R --vanilla --slave --args ${MODEL} 2 1 1 1 0.95 ${SPAB} ${DRY} ${NEIGHBOR} < run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}.log
 #R --vanilla --slave --args ${MODEL} 3000 2000 1 4 0.95 ${SPAB} ${DRY} < run_stan.r > ./log/${DRY}_stan_${SPAB}.log
