1e811fd9d0cc7ea5bd41bc6d1f3cbc812165c871
Masatoshi Katabuchi <mattocci27@gmail.com>
2020-06-28 23:00:28 +0800

    habitat model

--- sh/model_ind.sh
+++ sh/model_ind.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 SPAB=50
 MODEL=model_ind_gq
-for DRY in dry wet
+NEIGHBOR=full
+for DRY in dry rainy
 do
-  for TRAIT in C13 LT WP PCA
+  for TRAIT in C13 LT WP PCA2 PCA3 PCA4 C13-only LT-only WP-only StemD-only
   do
-    for NEIGHBOR in full seedling adult
+    for HAB in valley ridge slope
     do
       echo "Model for ${DRY} season. Min. species abundance = ${SPAB}"
       echo "Run ${MODEL}.stan"
       echo "Use ${NEIGHBOR} data"
       echo "Use ${TRAIT} for sp-level"
-      export SPAB DRY MODEL NEIGHBOR
+      echo "Habitat: ${HAB}"
+      export SPAB DRY MODEL NEIGHBOR HAB
         # n_iter n_warm n_thin n_chain adapt_delta n_ab season
-      nohup R --vanilla --slave --args ${MODEL} 2000 1000 1 4 0.95 ${SPAB} ${DRY} ${NEIGHBOR} ${TRAIT}< run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}_${TRAIT}.log &
+      nohup R --vanilla --slave --args ${MODEL} 2000 1000 1 4 0.95 ${SPAB} ${DRY} ${NEIGHBOR} ${TRAIT} ${HAB}< run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}_${TRAIT}_${HAB}.log &
+      #nohup R --vanilla --slave --args ${MODEL} 2 1 1 1 0.95 ${SPAB} ${DRY} ${NEIGHBOR} ${TRAIT} ${HAB}< run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}_${TRAIT}_${HAB}.log &
       sleep 1 # pause to be kind to the scheduler
     done
     wait
