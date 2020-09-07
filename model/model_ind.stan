d5205b3551a4fe1ec29b2e76a81831247dd31aa9
Masatoshi Katabuchi <mattocci27@gmail.com>
2020-09-07 14:20:44 +0800

    run model_ind.sh

--- model/model_ind.stan
+++ model/model_ind.stan
@@ -55,7 +55,11 @@ model {
 }
 
 generated quantities {
+  vector[N] log_lik;
+  vector[N] p;
   corr_matrix[K] Omega;
   Omega = multiply_lower_tri_self_transpose(L_Omega);
+  for (n in 1:N) {
+    log_lik[n] = bernoulli_logit_lpmf(suv[n] | dot_product(beta[sp[n],] , x[n,]) + phi[plot[n]] + xi[census[n]] + psi[tag[n]]);
+  }
 }
- 
--- test_stan.sh
+++ test_stan.sh
@@ -7,4 +7,5 @@ echo "Run ${MODEL}.stan"
 
 export SPAB DRY MODEL
   # n_iter n_warm n_thin n_chain adapt_delta n_ab season
-nohup R --vanilla --slave --args ${MODEL} 2000 1000 1 4 0.95 ${SPAB} ${DRY} < run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}.log &
+#nohup R --vanilla --slave --args ${MODEL} 2000 1000 1 4 0.95 ${SPAB} ${DRY} < run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}.log &
+nohup R --vanilla --slave --args ${MODEL} 2 1 1 1 0.95 ${SPAB} ${DRY} < run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}.log &
