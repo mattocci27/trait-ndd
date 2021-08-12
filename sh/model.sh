39654bd18c11e0af10d6c26a6252a5d65096dc8e
Masatoshi Katabuchi <mattocci27@gmail.com>
2021-08-12 13:26:17 +0900

    running interaction model

--- sh/model.sh
+++ sh/model.sh
@@ -2,14 +2,14 @@
 SPAB=50
 MODEL=model_inter
 NEIGHBOR=full
-for HAB in Full valley ridge slope
+for TRAIT in Full WD PCA
 #for HAB in valley
 do
-  for TRAIT in Full WD PCA
   #for TRAIT in PC2
+  for DRY in dry rainy
   do
     #for HAB in valley ridge slope all
-    for DRY in dry rainy
+    for HAB in Full valley ridge slope
     do
       echo "Model for ${DRY} season. Min. species abundance = ${SPAB}"
       echo "Run ${MODEL}.stan"
@@ -18,8 +18,8 @@ do
       echo "Habitat: ${HAB}"
       export SPAB DRY MODEL NEIGHBOR HAB TRAIT
         # n_iter n_warm n_thin n_chain adapt_delta n_ab season
-      #nohup R --vanilla --slave --args ${MODEL} 2000 1000 1 4 0.95 ${SPAB} ${DRY} ${NEIGHBOR} ${TRAIT} ${HAB}< run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}_${TRAIT}_${HAB}.log &
-      nohup R --vanilla --slave --args ${MODEL} 2 1 1 1 0.95 ${SPAB} ${DRY} ${NEIGHBOR} ${TRAIT} ${HAB}< run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}_${TRAIT}_${HAB}.log &
+      nohup R --vanilla --slave --args ${MODEL} 4000 2000 1 4 0.95 ${SPAB} ${DRY} ${NEIGHBOR} ${TRAIT} ${HAB}< run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}_${TRAIT}_${HAB}.log &
+     # nohup R --vanilla --slave --args ${MODEL} 2 1 1 1 0.95 ${SPAB} ${DRY} ${NEIGHBOR} ${TRAIT} ${HAB}< run_stan.r > ./log/${DRY}_stan_${SPAB}_${MODEL}_${NEIGHBOR}_${TRAIT}_${HAB}.log &
       sleep 1 # pause to be kind to the scheduler
       #wait
     done
