88fc827831ff67a3ab215db52c307236cd3fcc1f
Masatoshi Katabuchi <mattocci27@gmail.com>
2022-07-22 16:14:21 +0900

    update draws, summary, diagnostics

--- run.R
+++ run.R
@@ -1,11 +1,22 @@
-arg = commandArgs(T)
-
+# For general run
 targets::tar_make_clustermq(
-  workers = as.numeric(arg[1])
- # workers = parallel::detectCores()#,
-# reporter = "silent"
+  workers = parallel::detectCores(),
+  reporter = "silent"
 )
 
+# For single thread
 # targets::tar_make(
-# reporter = "silent"
+#   reporter = "silent"
+# )
+
+# For HPC
+# arg = commandArgs(T)
+# targets::tar_make_clustermq(
+#   workers = as.numeric(arg[1])
+# )
+
+# For building mcmc draws, diagnostics, summary
+# this needs huge RAM
+# targets::tar_make_clustermq(
+#   workers = 4
 # )
