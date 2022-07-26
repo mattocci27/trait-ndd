98f83f2ffde49bdc88fd17c09946807bd31dce6d
Masatoshi Katabuchi <mattocci27@gmail.com>
2022-07-26 21:11:02 +0800

    update meta

--- run.R
+++ run.R
@@ -1,8 +1,8 @@
 #For general run
-targets::tar_make_clustermq(
-   workers = parallel::detectCores()
+#targets::tar_make_clustermq(
+#   workers = parallel::detectCores()
 #   reporter = "silent"
-)
+#)
 
 #For single thread
 #targets::tar_make(
@@ -10,10 +10,10 @@ targets::tar_make_clustermq(
 #)
 
 # For HPC
-# arg = commandArgs(T)
-# targets::tar_make_clustermq(
-#   workers = as.numeric(arg[1])
-# )
+arg = commandArgs(T)
+targets::tar_make_clustermq(
+  workers = as.numeric(arg[1])
+)
 
 # For building mcmc draws, diagnostics, summary
 # this needs huge RAM
