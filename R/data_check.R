adb4962c3e88bb3cc8525fa4bfd3000f5a9aafc0
Masatoshi Katabuchi <mattocci27@gmail.com>
2022-08-04 12:26:02 +0900

    update ggpair

--- R/data_check.R
+++ R/data_check.R
@@ -6,13 +6,14 @@ trait_pair <- function(trait_log, full = TRUE) {
     rename(WD = wd) |>
     rename(SDMC = sdmc) |>
     rename(Chl = chl) |>
-    rename(C13 = c13) |>
+    rename("\u03b4C[13]" = c13) |>
+    rename("\u03c0[tlp]" = tlp) |>
     rename(C = c) |>
     rename(N = n) |>
     rename(CN = cn) |>
-    rename(`log[LA]` = log_la) |>
-    rename(`log[SLA]` = log_sla) |>
-    rename(`log[LT]` = log_lt)
+    rename(`ln~LA` = log_la) |>
+    rename(`ln~SLA` = log_sla) |>
+    rename(`ln~LT` = log_lt)
 
   if(!full) {
     trait_log <- trait_log  |>
@@ -22,8 +23,10 @@ trait_pair <- function(trait_log, full = TRUE) {
   trait_log  |>
     ggpairs(
       aes(alpha = 0.6),
-      upper = list(continuous = wrap("cor", size = 2.5))
+      upper = list(continuous = wrap("cor", size = 2.5)),
+      labeller = "label_parsed"
     ) +
+    theme_bw() +
     theme(text = element_text(family = "Arial", size = 6),
       strip.text = element_text(size = 8)
     )
