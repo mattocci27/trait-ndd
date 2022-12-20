13390c91aae6185b08fd0c6ecfb97052214e76bd
Masatoshi Katabuchi <mattocci27@gmail.com>
2022-12-20 15:20:07 +0900

    update bayes_check

--- test.R
+++ test.R
@@ -4,8 +4,7 @@ hoge <- tar_manifest()
 
 
 hoge |>
-  filter(str_detect(name, "loo"))
-
+  filter(str_detect(name, "diag"))
 
 mcmc_names <- expand_grid(a1 = c("phy", "het"), a2 = c("season", "rain"), a3 = c("ab", "ba", "both")) |>
   mutate(model = str_c("fit_mcmc_multilevel_logistic", a1, a2, a3, sep = "_")) |>
