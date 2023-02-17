f95bd39cc5da7c6aa1f2321093ddb81836af2cdc
Masatoshi Katabuchi <mattocci27@gmail.com>
2023-02-17 18:56:00 +0900

    clean

--- _targets.R
+++ _targets.R
@@ -185,29 +185,29 @@ main_ <- list(
   ),
 
 
-  tar_stan_mcmc(
-    check_ess,
-    "stan/logistic.stan",
-    data = stan_data_dry_het_intrain_ab,
-    refresh = 0,
-    chains = 4,
-    parallel_chains = getOption("mc.cores", 4),
-    iter_warmup = 2000,
-    iter_sampling = 2000,
-    adapt_delta = 0.9,
-    max_treedepth = 15,
-    seed = 123,
-    return_draws = TRUE,
-    return_diagnostics = TRUE,
-    return_summary = TRUE,
-    summaries = list(
-      mean = ~mean(.x),
-      sd = ~sd(.x),
-      mad = ~mad(.x),
-      ~posterior::quantile2(.x, probs = c(0.025, 0.05, 0.25, 0.5, 0.75, 0.95, 0.975)),
-      posterior::default_convergence_measures()
-    )
-  ),
+  # tar_stan_mcmc(
+  #   check_ess,
+  #   "stan/logistic.stan",
+  #   data = stan_data_dry_het_intrain_ab,
+  #   refresh = 0,
+  #   chains = 4,
+  #   parallel_chains = getOption("mc.cores", 4),
+  #   iter_warmup = 2000,
+  #   iter_sampling = 2000,
+  #   adapt_delta = 0.9,
+  #   max_treedepth = 15,
+  #   seed = 123,
+  #   return_draws = TRUE,
+  #   return_diagnostics = TRUE,
+  #   return_summary = TRUE,
+  #   summaries = list(
+  #     mean = ~mean(.x),
+  #     sd = ~sd(.x),
+  #     mad = ~mad(.x),
+  #     ~posterior::quantile2(.x, probs = c(0.025, 0.05, 0.25, 0.5, 0.75, 0.95, 0.975)),
+  #     posterior::default_convergence_measures()
+  #   )
+  # ),
 
   loo_map,
   tar_combine(
